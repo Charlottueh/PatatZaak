f1e96ec60f12f403c97bf4d058b98082e7b24f68
Charlottueh <lotteke.glaesmaekers@gmail.com>
2024-12-10 12:31:46 +0100

    continue order

---
+++ Models/Viewmodels/ConfirmationVM.cs
@@ -0,0 +1,14 @@
+﻿using PatatZaak.Models.Businesslayer;
+
+namespace PatatZaak.Models.Viewmodels
+{
+    public class ConfirmationVM
+    {
+       public int OrderNumber { get; set; }
+       public string? PickupNumber { get; set; }
+       public string? PickupTime { get; set; }
+       public decimal TotalPrice { get; set; }
+       public ICollection<Product>? Products { get; set; }
+      
+    }
+}
